f3b7a33e72d604efb8459add4844166e6e20a889
alaintresor <tresoralain35@gmail.com>
2025-07-18 10:19:34 +0200

    add link to card

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -7,12 +7,34 @@ export default function decorate(block) {
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     moveInstrumentation(row, li);
+
+    // Check if there's a link in this card
+    const linkElement = row.querySelector('a');
+    let linkWrapper = null;
+
+    if (linkElement && linkElement.href) {
+      // Create a wrapper link for the entire card
+      linkWrapper = document.createElement('a');
+      linkWrapper.href = linkElement.href;
+      linkWrapper.className = 'cards-card-link';
+
+      // Remove the original link element from the content
+      linkElement.remove();
+    }
+
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
       else div.className = 'cards-card-body';
     });
-    ul.append(li);
+
+    // If we have a link, wrap the li in the link wrapper
+    if (linkWrapper) {
+      linkWrapper.append(li);
+      ul.append(linkWrapper);
+    } else {
+      ul.append(li);
+    }
   });
   ul.querySelectorAll('picture > img').forEach((img) => {
     const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
