39590a2f83b8acb3083d7933ee8c765cd15d7eca
Melvin - Hashout <melvin-hashout@192.168.1.6>
2025-07-25 16:47:00 +0530

    added category list scrolling

--- blocks/category-listing/category-listing.js
+++ blocks/category-listing/category-listing.js
@@ -1,4 +1,43 @@
 export default function decorate(block) {
-  // eslint-disable-next-line no-console
-  console.log('block', block);
+  [...block.children].forEach((item) => {
+    const divs = item.querySelectorAll('div');
+    const link = divs[0]?.querySelector('a')?.href;
+    const label = divs[1]?.querySelector('p')?.textContent;
+
+    // Hide the raw link block
+    divs[0].style.display = 'none';
+
+    // if (link && label) {
+    //   const anchor = document.createElement('a');
+    //   anchor.href = link;
+    //   anchor.className = 'category-pill';
+    //   anchor.setAttribute('aria-label', label);
+    //   anchor.innerHTML = `
+    //     <span class="pill-text">${label}</span>
+    //     <span class="pill-arrow">›</span>
+    //   `;
+
+    //   divs[1].innerHTML = '';
+    //   divs[1].appendChild(anchor);
+    // }
+
+    if (label) {
+      const anchor = document.createElement(link ? 'a' : 'span');
+      if (link) {
+        anchor.href = link;
+        anchor.className = 'category-pill';
+        anchor.setAttribute('aria-label', label);
+      } else {
+        anchor.className = 'category-pill disabled';
+      }
+
+      anchor.innerHTML = `
+    <span class="pill-text">${label}</span>
+    <span class="pill-arrow">›</span>
+  `;
+
+      divs[1].innerHTML = '';
+      divs[1].appendChild(anchor);
+    }
+  });
 }
