7039d8ef8473525abfc35878c0c91688acc97a32
madukara <Madukara-webqem@aicd.com.au>
2025-07-30 12:53:52 +0530

    Revert Accordion block UI changes
    This reverts commit 5a4905473871bbb82953f08c1d974b897cd5d4a1.

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -5,12 +5,10 @@
  */
 
 import { moveInstrumentation } from '../../scripts/scripts.js';
-import scrollElementToTopAfterOpen from '../../scripts/utils.js';
 
 export default function decorate(block) {
-  const detailsList = [];
-
-  [...block.children].forEach((row, index) => {
+  [...block.children].forEach((row) => {
+    // decorate accordion item label
     const label = row.children[0];
     const summary = document.createElement('summary');
     summary.className = 'accordion-item-label';
@@ -23,40 +21,6 @@ export default function decorate(block) {
     moveInstrumentation(row, details);
     details.className = 'accordion-item';
     details.append(summary, body);
-
-    if (index === 0) {
-      details.setAttribute('open', '');
-    }
-
-    detailsList.push(details);
     row.replaceWith(details);
-  });
-
-  detailsList.forEach((detail) => {
-    const summary = detail.querySelector('summary');
-
-    summary.addEventListener('click', (e) => {
-      e.preventDefault();
-
-      const isOpen = detail.hasAttribute('open');
-
-      // If already open, close it
-      if (isOpen) {
-        detail.removeAttribute('open');
-        return;
-      }
-
-      // Close others
-      detailsList.forEach((d) => {
-        if (d !== detail) d.removeAttribute('open');
-      });
-
-      setTimeout(() => {
-        detail.setAttribute('open', '');
-
-        // Scroll after it's open
-        scrollElementToTopAfterOpen(detail);
-      }, 200); // Wait for others to collapse
-    });
   });
 }
