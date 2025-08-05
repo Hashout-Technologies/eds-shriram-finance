8cfdf0990afa3cee1b0ebe6528bfb86a6a31b984
Neelansh <neelansh.bansiwal@hashouttech.com>
2025-08-06 01:32:54 +0530

    created modified gold price

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,6 +1,6 @@
 import { fetchPlaceholders, getMetadata } from '../../scripts/aem.js';
 import { loadFragment } from '../fragment/fragment.js';
-
+import setupGoldPriceDropdown from './gold-price.js';
 // Constants
 const DESKTOP_BREAKPOINT = '(min-width: 900px)';
 const NAV_CLASSES = ['brand', 'sections', 'tools'];
@@ -1089,4 +1089,6 @@ export default async function decorate(block) {
     const breadcrumbs = await buildBreadcrumbs();
     navWrapper.append(breadcrumbs);
   }
+
+  setupGoldPriceDropdown();
 }
